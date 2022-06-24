819e6429635b07c4495bf9758b58d354969e677a
kiBROm <danu@localhost.localdomain>
2022-06-24 08:15:31 -0400

    revision 2 - doubly linked list

--- Doubly_LinkedList/Revision/DLinkedList.js
+++ Doubly_LinkedList/Revision/DLinkedList.js
@@ -2,6 +2,7 @@
  class DLinkedList {
     constructor() {
         this.head = null
+        this.tail = null
         this.length = 0
     }
 
@@ -10,22 +11,168 @@
             const newnode = new NewNode(null, val , null)
 
             this.head = newnode
+            this.tail = newnode
+
             this.length++
             return
         }
 
-        if(this.length === 1) {
             let oldhead = this.head
 
-            const newnode = new NewNode(null, val , this.head)
+            const newnode = new NewNode(null, val , oldhead)
 
 
-            this.head.prev = newnode
             this.head = newnode
+            oldhead.prev = this.head
             this.length++
-            return
 
+    }
+
+    insertAtTail = val => {
+        if(this.length === 0) return this.insertAtHead(val)
+
+        let currenthead = this.head;
+
+        while(currenthead.next) { currenthead = currenthead.next; }
+
+        const newnode = new NewNode(currenthead, val , null)
+        currenthead.next = newnode;
+        this.tail = newnode
+        this.length++
+    }
+
+    insertAtIndex = (index, val) => {
+        if(index < 0 || index >  this.length) return undefined
+        if(index === 0) return this.insertAtHead(val)
+        if(index === this.length) return this.insertAtTail(val)
+
+          let atindex = this.head;
+
+          for(let i = 1 ; i <= index ; i++) {
+            atindex = atindex.next
+          }
+
+          let previous = atindex.prev
+          let newnode = new NewNode(previous, val , atindex)
+          previous.next = newnode
+          atindex.prev = newnode
+
+          this.length++
+    }
+
+    removeAtHead = () => {
+        if(this.length === 0) return null
+
+        if(this.length === 1) {
+            this.head = null
+            this.tail = null
+
+            this.length = 0
+            return 0
         }
+
+       let newhead =  this.head.next
+       this.head = newhead
+       newhead.prev.next = null
+       this.head.prev = null
+       this.length--
+    }
+
+    removeAtIndex = index => {
+        if(index < 0 || index >= this.length) return undefined
+        if(this.length === 0) return null
+
+        if(this.length === 1) return this.removeAtHead()
+
+        let currenthead = this.head;
+
+     
+            for(let i = 1 ; i <= index ; i++) {
+                currenthead = currenthead.next;
+            }
+
+            if(index === this.length - 1) {
+                let previous = currenthead.prev
+                previous.next = null
+                currenthead.prev = null
+                this.tail = previous
+            }
+
+            else {
+    
+            let previous = currenthead.prev
+            previous.next = currenthead.next
+            currenthead.next.prev = previous.next
+            currenthead.next = currenthead.prev = null
+            }
+            this.length--
+
+
+        
+    }
+ 
+
+    getAtHead = () => { 
+        if(this.length === 0) return null
+        return this.head.val 
+    }
+
+    getAtTail = () => {
+        if(this.length === 0) return null
+        return this.tail.val 
+    }
+
+
+    getAtIndex = index => {
+
+        if(index >= this.length || index < 0) return undefined
+
+        if(index === 0 && (this.length >= 1) )
+         return this.getAtHead()
+
+        if(this.length === index + 1) return this.getAtTail()
+
+        if(this.length - index >= index ) {
+        let currenthead = this.head
+
+        for(let i = 1 ; i <= index ; i++) {  currenthead = currenthead.next; }
+
+        return currenthead.val;
+        }
+
+        else {
+            let currenttail = this.tail
+
+        for(let i = 1 ; i < this.length - index ; i++) 
+            {  currenttail = currenttail.prev; }
+
+        return currenttail.val;
+        }
+
+    }
+
+    print = () => {
+        if(this.length === 0)
+        { 
+            console.log( "Empty list \n -------------------------------\n")
+               return 
+        }
+        if(this.length === 1) {
+             console.log(`${this.head.val} -> null \n -------------------------------\n`)
+             return
+        }
+
+        let currenthead = this.head;
+        let output = "";
+
+        while(currenthead.next) { 
+            output += ` ${currenthead.val} -> `
+            currenthead = currenthead.next; 
+        }
+
+          output +=  currenthead.val + " -> null \n -------------------------------\n"
+
+          console.log(output)
     }
 
     getSize = () => { return this.length; }
--- Doubly_LinkedList/Revision/app.js
+++ Doubly_LinkedList/Revision/app.js
@@ -1,13 +1,89 @@
 
 const DLinkedList = require("./DLinkedList")
 
-  let dlist = new DLinkedList();
+  let dlisttwo = new DLinkedList();
 
-  dlist.insertAtHead(10)
+  dlisttwo.insertAtHead(10)
+  dlisttwo.removeAtHead()
 
-  console.log(dlist)
+  console.log(dlisttwo)
+    dlisttwo.print()
 
-  dlist.insertAtHead(10)
-  console.log(dlist)
+  // dlist.insertAtHead(20)
+  // console.log(dlist)
 
-    console.log(dlist.getSize())
+  //     dlist.print()
+
+
+
+  // dlist.insertAtHead(30)
+  // console.log(dlist)
+
+  //     dlist.print()
+
+
+
+  let dlist = DLinkedList.fromArray(10,20,30,40,50)
+
+      dlist.print()
+
+  dlist.insertAtTail(99)
+  dlist.insertAtTail(100)
+  dlist.insertAtTail(101)
+  dlist.insertAtTail(102)
+
+
+
+      dlist.insertAtIndex(2,55)
+      dlist.insertAtIndex(5,65)
+      dlist.insertAtIndex(7,505)
+       dlist.insertAtIndex(9,550)
+       dlist.insertAtIndex(0,225)
+       dlist.insertAtIndex(-1,225)
+       dlist.insertAtIndex(16,225)
+       dlist.insertAtIndex(14,333)
+
+
+
+      console.log(dlist)
+
+      dlist.print()
+
+      dlist.removeAtIndex(2)
+      dlist.print()
+      console.log(dlist)
+
+      dlist.removeAtIndex(13)
+      dlist.print()
+      console.log(dlist)
+
+      dlist.removeAtIndex(12)
+      dlist.print()
+      console.log(dlist)
+
+      dlist.removeAtIndex(8)
+      dlist.print()
+
+
+ console.log(dlist.getAtHead())
+ console.log(dlist.getAtTail())
+ console.log(dlist.getAtIndex(3))
+ console.log(dlist.getAtIndex(13))
+ console.log(dlist.getAtIndex(9))
+ console.log(dlist.getAtIndex(10))
+ console.log(dlist.getAtIndex(11))
+ console.log(dlist.getAtIndex(1))
+ console.log(dlist.getAtIndex(2))
+ console.log("----------------")
+ console.log(dlist.getAtIndex(7))
+ console.log(dlist.getAtIndex(6))
+ console.log(dlist.getAtIndex(5))
+ console.log(dlist.getAtIndex(8))
+
+ console.log(dlist.getAtIndex(-1))
+ console.log(dlist.getAtIndex(0))
+
+
+
+
+
